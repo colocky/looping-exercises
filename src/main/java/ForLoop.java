9a2ae463bab71b4d8fa041b935448fa0865662c5
colocky <callmeadam.ar@gmail.com>
2026-04-15 11:09:51 -0500

    Completed: Exercises - Looping: Step 2

---
+++ src/main/java/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int iLoveJavas = 0; iLoveJavas < 5; iLoveJavas++) {
+            System.out.println("I Love Java!");
+        }
+    }
+}
