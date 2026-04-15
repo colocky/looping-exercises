12986e2627a8028612c4d1c38dd90bbfa1dff900
colocky <callmeadam.ar@gmail.com>
2026-04-15 11:04:36 -0500

    Completed: Exercises - Looping

---
+++ src/main/java/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int iLoveJavas = 0;
+
+        do {
+            System.out.println("I Love Java!");
+            iLoveJavas++;
+        } while (iLoveJavas < 5);
+
+
+    }
+}
---
+++ src/main/java/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int iLoveJavas = 0;
+        while (iLoveJavas < 5) {
+            System.out.println("I Love Java!");
+            iLoveJavas++;
+        }
+    }
+}
